5691ffd2a9f887c86db77ed2df0c49e753a80ba8
Jai <jaijai.stell@gmail.com>
2023-03-21 12:18:47 -0700

    complete lab yay

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+// jais code
+//uhhhhhh
+var customerName = "bob";
+//step 1 done yay
+function upperCaseCustomerName() {
+  return (customerName = customerName.toUpperCase());
+}
+//make uppercaase
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+//lol
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+//needed
+const leastFavoriteCustomer = " ";
+// rip ted
+function changeLeastFavoriteCustomer() {
+  return (leastFavoriteCustomer = " ");
+}
+//yay purposeful uncessful
